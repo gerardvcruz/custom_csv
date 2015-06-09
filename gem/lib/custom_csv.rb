98ae04a18cc2741cf7a7e819e9a9cfc1d1305a74
Gerard Cruz <gerardcruz@live.com>
2015-06-09 19:44:21 +0800

    refactor code

--- gem/lib/custom_csv.rb
+++ gem/lib/custom_csv.rb
@@ -1,66 +1,72 @@
-def generate_csv(model, filters, options)
-  @csv = ""
-  @model = model.classify.constantize
+module CustomCSV
+  class Generator
+    def generate_csv(model, options)
+      @csv = "" # the csv, yay!
+      @model = model.classify.constantize # convert model arg to Class
 
-  @model.all.each do |model_object|
-    if options.nil?
-      # by default, load all params from database
-      @headers = @model.column_names
-      @headers.each do |column_header|
-        if !(column_header == @arr[-1])
-          @csv << "\"#{column_header.capitalize.gsub("_", " ")}\","
-        else
-          @csv << "\"#{column_header.capitalize.gsub("_", " ")}\"\n"
-        end
-      end
+      # does the model exist
+      if @model.exists?
+        add_csv_headers options
 
-      @model.all.each do |model_object|
-        if !(element == @arr[-1])
-          @csv << "\"#{model_object.send(element).to_s}\","
-        else
-          @csv << "\"#{model_object.send(element).to_s}\"\n"
-        end
-      end
-
-    else
-      @headers = options
-      @headers.each do |column_header|
-        if !(column_header == @arr[-1])
-          @csv << "\"#{column_header.capitalize.gsub("_", " ")}\","
-        else
-          @csv << "\"#{column_header.capitalize.gsub("_", " ")}\"\n"
-        end
-      end
-
-
-      @model.all.each do |model_object|
-        @headers.each do |column_header|
-          # uses markers such as ":" and "%" to check if
-          # user wants to retrieve data from external model
-
-          # ":" (colon) signifies parent model
-          if column_header.include?(":")
-            parent_model = column_header.partition(':').first
-            parent_column = column_header.partition(':').third
-
-            parent_model = parent_model.classify.constantize
-            @csv << "\"#{parent_model.first.send(parent_column).to_s}\","
-
-          # "%" (percent) signifies child model
-          elsif column_header.include?("%")
-            child_model = column_header.partition('%').first
-            child_column = column_header.partition('%').third
-
-            child_model = child_model.classify.constantize
-            @csv << "\"#{child_model.first.send(child_column).to_s}\","
-
-          else
-            @csv << "\"#{model_object.send(column_header).to_s}\","
+        unless options.empty?
+          @model.all.each do |model_object|
+            @headers.all.each do |header|
+              @csv << "\"#{model_object.send(header).to_s}\","
+              !(header == @arr[-1]) ? @csv << "," : @csv << "\n"
+            end
           end
         end
+
+        @csv
+      else
+        raise "#{@model} doesn't exist!"
       end
     end
 
-    @csv
+    private
+      def add_model_headers options
+        # load ALL the model column names!
+        @headers = @model.column_names
+
+        unless options.empty?
+          includes = options[:includes]
+          excludes = options[:excludes]
+
+          unless excludes.empty?
+            @headers.delete_if { |header| excludes.include? header }
+          end
+
+          unless includes.empty?
+            unless includes[:parent].empty?
+              includes[:parent].each do |parent|
+                if @model.try(parent).nil?
+                  raise "No relationship found: #{@model}.#{parent}"
+                else
+                  @headers.insert 0, parent
+                end
+              end
+            end
+
+            unless includes[:child].empty?
+              includes[:child].each do |child|
+                if @model
+                  .try(child.pluralized == child ? child : child.pluralized)
+                  .nil?
+
+                  raise "No relationship found: #{@model}.#{child}"
+                else
+                  @headers << child
+                end
+              end
+            end
+          end
+        end
+
+        @headers.each do |column_header|
+          @csv << "\"#{column_header.capitalize.gsub("_", " ")}\""
+          !(column_header == @arr[-1]) ? @csv << "," : @csv << "\n"
+        end
+      end
+
   end
 end
